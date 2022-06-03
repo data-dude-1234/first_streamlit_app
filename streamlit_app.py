a2d6b0672081217b89d2b16157f98da4c2c95306
DP <106685539+data-dude-1234@users.noreply.github.com>
2022-06-03 09:45:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
 import snowflake.connector
+from urllib.error import URLError
+import requests
 
 
 
@@ -25,7 +27,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit whould you like information about?', 'Kiwi')
 streamlit.write('The user entered', fruit_choice)
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json()) # Just writes the data to the screen
 
@@ -33,6 +35,8 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #ouptut it to the screen as a table
 streamlit.dataframe(fruityvice_normalized)
+
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -42,3 +46,5 @@ streamlit.header("The Fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit whould you like to add', 'jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
+#my_cur.execute("insert into fruit_load_list values ('from stremlit')")
+
